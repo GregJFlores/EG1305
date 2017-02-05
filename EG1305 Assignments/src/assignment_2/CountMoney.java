a42ba36596bc54213e843564fd39c2240172fe6c
gflores21 <gflores21@mail.stmarytx.edu>
2017-02-04 23:37:42 -0600

    corrected inefficiency in algorithm

--- EG1305 Assignments/src/assignment_2/CountMoney.java
+++ EG1305 Assignments/src/assignment_2/CountMoney.java
@@ -40,13 +40,17 @@ public class CountMoney
 		 * Fourth- parse the string cents to an integer then subtract the amount of quarters, subtract the amount of dimes,
 		 * subtract the amount of nickels, finally divide by the amount of a penny and assign to pennies.
 		 */
-		int quarters = Integer.parseInt(cents)/25;
-		int dimes = (Integer.parseInt(cents)-(quarters*25))/10;
-		int nickels = (Integer.parseInt(cents)-(dimes*10)-(quarters*25))/5;
-		int pennies = (Integer.parseInt(cents)-(nickels*5)-(dimes*10)-(quarters*25))/1;
+		//corrected inefficiency 
+		int parsedCents = Integer.parseInt(cents);
+		int quarters = parsedCents/25;
+		int dimes = parsedCents-(quarters*25))/10;
+		int nickels = parsedCents-(dimes*10)-(quarters*25))/5;
+		int pennies = parsedCents-(nickels*5)-(dimes*10)-(quarters*25))/1;
 		
 		//The algorithm is correct, but it parses the input 4 times, which is a bit inefficient. 
 		//Save Integer.parseInt(cents) to a separate variable before counting.
+		
+		//corrected inefficency 
 		
 		//output result to console
 		System.out.println("You entered "+currency+" which is $"+dollars+"."+cents
